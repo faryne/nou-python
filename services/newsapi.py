d7d7c399ba2856f5b3c568a43cb177aaf9c1584c
Dichotomania <dichotomania@hotmail.com>
2023-04-21 17:17:37 +0800

    更新回傳欄位

--- services/newsapi.py
+++ services/newsapi.py
@@ -2,6 +2,12 @@
 # 第一個參數會是 newsapi 所需要的 apikey
 # 第二個參數則是要搜尋的關鍵字
 
+#'title': 新聞標題 , 
+#'content': 新聞摘要 ,
+#'url': 網址 , 
+#'publishedtime': 發布時間 
+
+from flask import jsonify
 import requests
 import json
 
@@ -13,44 +19,56 @@ apibaseurl = 'https://newsapi.org/v2/'
 # date_end = '2023-03-30'
 
 #搜尋頭條新聞
-def search_top(apikey: str, keyword: str):
+def search_top(keyword):
     url = apibaseurl + 'top-headlines'
     params = {'q': keyword, 'apiKey': apikey, 'country': 'tw' }
     response = requests.get(url, params=params)
     return json.loads(response.text)
 
 #搜尋所有新聞
-def search(apikey: str, keyword: str):
+def search(keyword):
     url = apibaseurl + 'everything'
     params = {'q': keyword, 'apiKey': apikey}
     response = requests.get(url, params=params)
-    return json.loads(response.text)
+    if response.status_code == 200:
+        json_response = json.loads(response.text)
+        # print(json_response)
+        news_lists = []
+        for article in json_response['articles']:
+            news = {
+                'title': article['title'],
+                'content': article['content'],
+                'url': article['url'],
+                'publishedtime': article['publishedAt'],
+            }
+            news_lists.append(news)
+        return news_lists
+    else:
+        return None
 
 if __name__ == '__main__':     
-    result = search(apikey, '星宇航空')
-
-    #回傳狀態
-    print(result['status'])
-
-    #總共有幾則新聞
-    print(result['totalResults'])
-
-    #articles文章陣列  
-    for article in result['articles']:
-        #來源
-        print(article['source']['name'])
-        #作者
-        print(article['author'])
-        #標題
-        print(article['title'])
-        #內容
-        print(article['content'])
-        #網址
-        print(article['url'])
-        #圖片
-        print(article['urlToImage'])
-        #發布時間
-        print(article['publishedAt'])
-        #來源
-        print(article['source']['name'])
-        print('-----------------------')
+    result = search('郭台銘')
+    print(result)
+    # #回傳狀態
+    # print(result['status'])
+    # #總共有幾則新聞
+    # print(result['totalResults'])
+    # #articles文章陣列  
+    # for article in result['articles']:
+    #     #來源
+    #     print(article['source']['name'])
+    #     #作者
+    #     print(article['author'])
+    #     #標題
+    #     print(article['title'])
+    #     #內容
+    #     print(article['content'])
+    #     #網址
+    #     print(article['url'])
+    #     #圖片
+    #     print(article['urlToImage'])
+    #     #發布時間
+    #     print(article['publishedAt'])
+    #     #來源
+    #     print(article['source']['name'])
+    #     print('-----------------------')
