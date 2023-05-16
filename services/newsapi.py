8a748947799d624bf43e1c0cdce0a9d0b25e4ea4
Dichotomania <dichotomania@hotmail.com>
2023-05-16 23:17:10 +0800

    更新search只取20筆資料,刪除多餘print,更新search_top回傳格式

--- services/newsapi.py
+++ services/newsapi.py
@@ -20,19 +20,40 @@ apibaseurl = 'https://newsapi.org/v2/'
 #搜尋頭條新聞
 def search_top(keyword):
     url = apibaseurl + 'top-headlines'
-    params = {'q': keyword, 'apiKey': apikey, 'country': 'tw' }
+    params = {'q': keyword, 
+            'pageSize': 20,
+            'apiKey': apikey,
+            'country': 'tw' }
     response = requests.get(url, params=params)
-    return json.loads(response.text)
+    if response.status_code == 200:
+        json_response = json.loads(response.text)
+        news_lists = []
+        # print(len(json_response['articles']))
+        for article in json_response['articles']:
+            news = {
+                'title': article['title'],
+                'content': article['content'],
+                'url': article['url'],
+                'publishedtime': article['publishedAt'],
+            }
+            news_lists.append(news)
+        return news_lists
+    else:
+        return None    
 
 #搜尋所有新聞
 def search(keyword):
     url = apibaseurl + 'everything'
-    params = {'q': keyword, 'apiKey': apikey}
+    params = {'q': keyword, 
+              'pageSize': 20,  #分頁設定傳回20筆資料, 
+              'page': 1,       #傳回第一頁的資料
+              'sortBy': 'publishedAt', #以發布時間排序
+              'apiKey': apikey}
     response = requests.get(url, params=params)
     if response.status_code == 200:
         json_response = json.loads(response.text)
-        # print(json_response)
         news_lists = []
+        print(len(json_response['articles']))
         for article in json_response['articles']:
             news = {
                 'title': article['title'],
@@ -45,29 +66,6 @@ def search(keyword):
     else:
         return None
 
-if __name__ == '__main__':     
-    result = search('郭台銘')
-    print(result)
-    # #回傳狀態
-    # print(result['status'])
-    # #總共有幾則新聞
-    # print(result['totalResults'])
-    # #articles文章陣列  
-    # for article in result['articles']:
-    #     #來源
-    #     print(article['source']['name'])
-    #     #作者
-    #     print(article['author'])
-    #     #標題
-    #     print(article['title'])
-    #     #內容
-    #     print(article['content'])
-    #     #網址
-    #     print(article['url'])
-    #     #圖片
-    #     print(article['urlToImage'])
-    #     #發布時間
-    #     print(article['publishedAt'])
-    #     #來源
-    #     print(article['source']['name'])
-    #     print('-----------------------')
+if __name__ == '__main__':
+    result = search('國民黨')
+    # print(result)
